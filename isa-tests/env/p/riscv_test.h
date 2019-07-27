2856ef890ef3d67efd29c92b1ba7b6e0bfbc5d3d
Tomas Brabec <xtbr123@gmail.com>
2019-07-27 14:35:36 +0200

    Removed access to undefined CSR from ISA tests.
    The ISA test environment was borrowed from e200_opensource. This commit
    removes some of the code specific to e203 core, which caused ibex to (newly)
    rise illegal instruction.

--- isa-tests/env/p/riscv_test.h
+++ isa-tests/env/p/riscv_test.h
@@ -283,8 +283,8 @@ post_mtvec:  la t0, stvec_handler;                                           \
         /*Bob added code to enable the interrupt enables: begin*/              \
         li a0, MSTATUS_MPIE;                                                   \
         csrs mstatus, a0;                                                     \
-        li a0, 0x0;/*We need to enable the ecc exception */                                                   \
-        csrw 0xbfc, a0;                                                     \
+/*        li a0, 0x0;/*We need to enable the ecc exception */                                                   \
+/*        csrw 0xbfc, a0;*/                                                     \
         /*Bob added code to enable the interrupt enables: end*/              \
         /*Bob added code to enable the PLL: begin{*/                    \
 /*        li t0, 2560; /*Wait loops before switch to PLL*/                  \
